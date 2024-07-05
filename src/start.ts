e77983965bb6cef9a8e01325dfe95f6dbcadc48c
KimInteger <kjs20151240@gmail.com>
2024-07-05 17:52:15 +0900

    edit_김정수 : start.ts에 파일조립 완료

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,18 @@
 import './style/start.css';
 
 import hOneTag from './module/hOneTag';
+import makeDiv from './module/makeDiv';
+import setID from './module/setId';
+import makeForm from './module/makeForm';
 
 const root: HTMLElement = document.getElementById('root') as HTMLDivElement;
 
 function plusAll(elements: string): void {
   root.innerHTML = hOneTag(elements);
+  let linkDiv = makeDiv();
+  setID(linkDiv, 'link');
+  makeForm(linkDiv);
+  root.appendChild(linkDiv);
 }
 
 document.addEventListener('DOMContentLoaded', (): void => {
