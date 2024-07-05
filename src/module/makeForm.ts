5b3e0a956de85a314091bb002296a6b3fc433d2f
KimInteger <kjs20151240@gmail.com>
2024-07-05 16:46:14 +0900

    docs_김정수 : 오타수정

--- src/module/makeForm.ts
+++ src/module/makeForm.ts
@@ -1,7 +1,9 @@
 import setID from './setId';
 
-function makeForm(innerHtml: string): void {
+function makeForm(innerHtml: string): HTMLFormElement {
   let result: HTMLFormElement = document.createElement('form');
   setID(result, 'start-form');
   result.innerHTML = innerHtml;
+
+  return result;
 }
