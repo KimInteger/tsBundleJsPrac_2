0e1a6e8ba1a4d759c9c58765700b0d4ad234fa2c
KimInteger <kjs20151240@gmail.com>
2024-07-05 12:53:27 +0900

    test_김정수 : test용 ts파일

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,14 @@
-console.log('start.ts');
+const root: HTMLElement = document.getElementById('root') as HTMLDivElement;
+
+function hOneTag(textNode: string): string {
+  let result: string = `<h1>${textNode}</h1>`;
+  return result;
+}
+
+function plusAll(elements: string): void {
+  root.innerHTML = hOneTag(elements);
+}
+
+document.addEventListener('DOMContentLoaded', (): void => {
+  plusAll('hellow script');
+});
