3ab400517362a7a27c2d7325ff86fd496461ed81
KimInteger <kjs20151240@gmail.com>
2024-07-05 17:45:21 +0900

    edit_김정수 : Form조립식으로 변경

--- src/module/makeForm.ts
+++ src/module/makeForm.ts
@@ -1,9 +1,32 @@
 import setID from './setId';
+import fusionCont from './fusionCont';
+import makeLabel from './makelabel';
+import makeInputName from './makeInputWithName';
+import makeInputValue from './makeInputWithValue';
 
-function makeForm(innerHtml: string): HTMLFormElement {
+function makeForm(element: HTMLDivElement): void {
   let result: HTMLFormElement = document.createElement('form');
   setID(result, 'start-form');
-  result.innerHTML = innerHtml;
-
-  return result;
+  result.appendChild(
+    fusionCont(
+      'form-group',
+      makeLabel('id', 'ID'),
+      makeInputName('text', 'id', 'id'),
+    ),
+  );
+  result.appendChild(
+    fusionCont(
+      'form-group',
+      makeLabel('name', 'NAME'),
+      makeInputName('text', 'name', 'name'),
+    ),
+  );
+  result.appendChild(
+    fusionCont(
+      'button-group',
+      makeInputValue('submit', '게임시작', 'start'),
+      makeInputValue('button', '게임종료', 'exit'),
+    ),
+  );
+  element.appendChild(result);
 }
